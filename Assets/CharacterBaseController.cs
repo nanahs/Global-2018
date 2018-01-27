4cd81dc347fc3a1ce21ee480ed2fb021da68e0a2
that-guy-is-it <that.guy.is.it@gmail.com>
2018-01-26 22:33:54 -0500

    Added a tag and built a scene
    Yadda yadda built a test scene. Basically a proof of concept.

---
+++ Assets/CharacterBaseController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterBaseController : MonoBehaviour {
+
+	public float speed = 1.0f;
+	public bool nearVacuum = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Move ();
+		takeControl ();
+
+	}
+
+	void Move(){
+		if(Input.GetKey(KeyCode.D)){
+			transform.Translate (Vector2.right * Time.deltaTime * speed);
+		}
+		if(Input.GetKey(KeyCode.A)){
+			transform.Translate (Vector2.right * Time.deltaTime * speed * -1);
+		}
+	}
+
+	void takeControl(){
+		if (Input.GetKeyDown (KeyCode.S)) {
+			RaycastHit hit;
+			Ray ray = new Ray (transform.position, Vector2.right);
+			if (Physics.Raycast (transform.position, Vector2.right, 5)) {
+				Physics.Raycast (ray, out hit);
+				if (hit.collider.CompareTag("VacuumCleaner")) {
+					nearVacuum = true;
+					gameObject.SetActive (false);
+					//Vector3 spawnPoint = new Vector3 (hit.collider.transform.position.x, hit.collider.transform.position.y + 0.5, hit.collider.transform.position.z);
+					transform.position = hit.collider.transform.position;
+
+
+				}
+
+			}
+				
+		}
+	}
+
+
+}
