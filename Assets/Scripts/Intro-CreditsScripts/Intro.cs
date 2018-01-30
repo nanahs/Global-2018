ee7098991932ca0c293898ac8eb99d622e4c1731
Shanan Dalton <shanan_dalton@yahoo.com>
2018-01-30 05:15:41 -0500

    Better camera, no more stuck on credits

--- Assets/Scripts/Intro-CreditsScripts/Intro.cs
+++ Assets/Scripts/Intro-CreditsScripts/Intro.cs
@@ -18,6 +18,11 @@ public class Intro : MonoBehaviour {
     // Update is called once per frame
     void Update() {
 
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
     }
 
     public void ShowText()
